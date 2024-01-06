30846e12ee716b56a46e91a88e435cb1a2e33f65
Dewciu <kacperkrol@protonmail.com>
2024-01-06 17:13:33 +0100

    boilerplate for image render

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, render_template
 from app.auth.routes import bp as auth_bp
+from app.charts.routes import bp as charts_bp
 from flask_login import current_user
 
 bp = Blueprint("index", __name__)
@@ -8,6 +9,7 @@ bp = Blueprint("index", __name__)
 def make_routes(app: Flask) -> None:
     app.register_blueprint(bp)
     app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(charts_bp, url_prefix="/charts")
 
 
 @bp.route("/")
