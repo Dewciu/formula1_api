6247abe3b99bb5fed31a7dd095b4eb04aa2a2539
Dewciu <kacperkrol@protonmail.com>
2024-01-06 17:41:01 +0100

    fix bugs

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -3,7 +3,7 @@ from flask_bcrypt import Bcrypt
 
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
-login_manager.login_view = "login"
+login_manager.login_view = "auth.login"
 login_manager.login_message_category = "info"
 
 bcrypt = Bcrypt()
