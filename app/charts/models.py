c54944fa5302a4864bc2820e9a077de59ad7af98
Dewciu <kacperkrol@protonmail.com>
2024-01-06 20:33:52 +0100

    prettify and make images single view

---
+++ app/charts/models.py
@@ -0,0 +1,11 @@
+from app.database import db
+
+
+class Chart(db.Model):
+    __tablename__ = "charts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    filename = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(10000), nullable=False)
+    created_at = db.Column(db.TIMESTAMP, default=db.func.current_timestamp())
--- app/charts/routes.py
+++ app/charts/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from app.charts.models import Chart
 
 bp = Blueprint("charts", __name__)
 
@@ -7,4 +8,18 @@ bp = Blueprint("charts", __name__)
 @bp.route("/")
 @login_required
 def index():
-    return render_template("charts/list.html")
+    charts = Chart.query.all()
+    return render_template(
+        "charts/list.html",
+        charts=charts,
+    )
+
+
+@bp.route("/chart/<int:chart_id>")
+@login_required
+def chart_details(chart_id):
+    chart = Chart.query.get(chart_id)
+    return render_template(
+        "charts/single.html",
+        chart=chart,
+    )
